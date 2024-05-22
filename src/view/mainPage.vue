805e6afb7a7b64c36a3c325e067855e88ad18d20
unknown <malak012426@gmail.com>
2024-05-22 22:43:25 +0000

    before trying with icons

--- src/view/mainPage.vue
+++ src/view/mainPage.vue
@@ -10,30 +10,35 @@
             class="d-inline-block align-top"
           />
         </a>
-        <div class="d-flex ms-auto">
+
+        <div class="d-flex ms-auto ">
+          
           <router-link to="/" class="btn btn-outline-dark">Logout</router-link>
         </div>
       </div>
     </nav>
-    <nav class="navbar navbar-light bg-light searchbar">
+    <nav class="navbar navbar-light searchbar">
       <div class="container-fluid">
-        <form class="d-flex">
+        <form class="d-flex mx-auto" style="width: 100%; max-width: 600px">
           <input
-            class="form-control me-2"
+            class="form-control me-2 flex-grow-1"
             type="search"
             placeholder="Search"
             aria-label="Search"
           />
-          <button class="btn btn-outline-success" type="submit">Search</button>
+          <button class="btn btn-outline-dark" type="submit">Search</button>
         </form>
       </div>
     </nav>
+
+    
   </div>
 </template>
 
 <script></script>
 <style>
-.searchbar{
-    margin-top: 70px;
+.searchbar {
+  margin-top: 90px;
+  justify-content: center;
 }
 </style>
